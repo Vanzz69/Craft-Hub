93af36e1a4425fa796f7cee0450f17b8c1939275
Vanzz69 <vanshnarayantiwari5@gmail.com>
2026-04-10 10:28:22 +0530

    Fix relative paths for vite build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 // Vite config to ensure it outputs all your pages properly and binds to your GitHub repo name
 export default defineConfig({
@@ -7,10 +6,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        products: resolve(__dirname, 'pages/products.html'),
-        seller: resolve(__dirname, 'pages/seller.html'),
-        cart: resolve(__dirname, 'pages/cart.html'),
+        main: './index.html',
+        products: './pages/products.html',
+        seller: './pages/seller.html',
+        cart: './pages/cart.html',
       }
     }
   }
